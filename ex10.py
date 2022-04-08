d99e0ac0ca497c22f5d23536cfb1e8498d53d7c5
SmittydoesCode <davwilsm1998@yahoo.com>
2022-04-08 13:27:37 -0500

    Ex10 change.

---
+++ ex10.py
@@ -0,0 +1,27 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+print("*****************")
+
+print("Here is a study drill.")
+print("I'm going to write some code with:\n\t*Formats\n\t*Escape Characters")
+
+my_name = 'Will Smith'
+my_hobbies = "\n\t*guitar\n\t*coding\n\t*gaming"
+
+print(f"My name is: {my_name}")
+print(f"My hobbies include:{my_hobbies}")
+
+mission = "Success!"
+print(f"My mission was a {mission}")
