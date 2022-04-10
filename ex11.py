6e7b7acb3f652aa2410c7f7d691cae1f0f5771df
SmittydoesCode <davwilsm1998@yahoo.com>
2022-04-10 18:59:14 -0500

    Exercises 11-12

---
+++ ex11.py
@@ -0,0 +1,17 @@
+print("How old are you?", end = ' ')
+age = input()
+print("How tall are you?", end = ' ')
+height = input()
+print("How much do you weigh?", end = ' ')
+weight = input()
+
+print(f"So you're {age} years old, {height} tall and {weight} heavy.")
+
+print("So here is a study drill...")
+
+print("Please enter your email")
+email = input("Enter email : ")
+print("To confirm, email:", email)
+print("Is this correct?")
+YN = input()
+print(f"You answered {YN}.\nProceeding with program.")
